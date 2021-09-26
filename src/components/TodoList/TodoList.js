575b5f2b890620eb03ef3eb60bac1acf83d197e5
Kirsten Green <hello@kirstengreen.com>
2021-09-26 16:39:49 -0400

    removed default list item style

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -4,7 +4,7 @@ import TodoItem from '../TodoItem/TodoItem'
 export default function TodoList( {todos} ) {
 
   return (
-    <ul>
+    <ul className='todo_list_container'>
       {todos.map( (todo, i) => (
         <TodoItem key={i} name={todo.name}/>
       ))}
