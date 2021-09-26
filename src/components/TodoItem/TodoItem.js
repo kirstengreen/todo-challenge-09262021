e39d47299086c5b9dc2f6cc12df9995ec3d6341f
Kirsten Green <hello@kirstengreen.com>
2021-09-26 18:13:56 -0400

    moved key up from TodoItem > TodoList to correct key prop error in console

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -2,7 +2,7 @@ import './TodoItem.css'
 
 export default function TodoItem( {todo, deleteTodo} ) {
   return (
-    <li key={todo.id} className='todo_item'>
+    <li className='todo_item'>
       <p>{todo.name}</p>
       <p><img src='/img/icon__close.svg' alt='delete icon' onClick={ () => deleteTodo(todo.id) }/></p>
     </li>
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -6,7 +6,7 @@ export default function TodoList( {todos, deleteTodo} ) {
   return (
     <ul className='todo_list_container'>
       {todos.map( (todo) => (
-        <TodoItem todo={todo} deleteTodo={deleteTodo}/>
+        <TodoItem key={todo.id} todo={todo} deleteTodo={deleteTodo}/>
       ))}
     </ul>
   )
