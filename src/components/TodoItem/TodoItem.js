4ab764781711cbedce373f2c67586152d74f2d41
Kirsten Green <hello@kirstengreen.com>
2021-09-26 17:35:00 -0400

    added flexbox and delete icon to todo items component

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,7 +1,10 @@
+import './TodoItem.css'
+
 export default function TodoItem( {key, name} ) {
   return (
-    <li key={key}>
-      {name}
+    <li key={key} className='todo_item'>
+      <p>{name}</p>
+      <p><img src='/img/icon__close.svg' alt='delete icon' /></p>
     </li>
   )
 }
