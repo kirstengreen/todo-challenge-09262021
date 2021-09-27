bb7bb1c34d266bbc57545fddbf715401ac0eb822
Kirsten Green <hello@kirstengreen.com>
2021-09-27 13:11:21 -0400

    fixed a bug where the wrong element was assigned onClick function for todo list

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -3,10 +3,10 @@ import './TodoItem.css'
 export default function TodoItem( {todo, deleteTodo, toggleComplete} ) {
   return (
     <li className='todo_item_container'>
-      <div className='todo_item' onClick={ () => toggleComplete(todo.id) }>
+      <div className='todo_item'>
         <label htmlFor={todo.name}>
           {todo.name}
-          <input type='checkbox' name={todo.name} id={todo.name} />
+          <input type='checkbox' name={todo.name} id={todo.name} onClick={ () => toggleComplete(todo.id) }/>
           <span className='checkbox'></span>
         </label>
       </div>
