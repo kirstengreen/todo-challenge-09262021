4b1699c415a61f3cbe51886c478835781e4c6388
Kirsten Green <hello@kirstengreen.com>
2021-09-27 09:11:01 -0400

    refactored delete icon and added hover styles

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -10,7 +10,11 @@ export default function TodoItem( {todo, deleteTodo} ) {
           <span className='checkbox'></span>
         </label>
       </div>
-      <img src='/img/icon__close.svg' alt='delete icon' onClick={ () => deleteTodo(todo.id) } />
+      <div className='delete_icon' onClick={ () => deleteTodo(todo.id) }>
+        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" />
+        </svg>
+      </div>
     </li>
   )
 }
