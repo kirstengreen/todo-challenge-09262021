fb0352545c5fdebc72a12e630bf198d97248e656
Kirsten Green <hello@kirstengreen.com>
2021-09-26 23:43:56 -0400

    refactored todo item component and added styles

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -2,9 +2,15 @@ import './TodoItem.css'
 
 export default function TodoItem( {todo, deleteTodo} ) {
   return (
-    <li className='todo_item'>
-      <p>{todo.name}</p>
-      <p><img src='/img/icon__close.svg' alt='delete icon' onClick={ () => deleteTodo(todo.id) }/></p>
+    <li className='todo_item_container'>
+      <div className='todo_item'>
+        <label htmlFor={todo.name}>
+          {todo.name}
+          <input type='checkbox' name={todo.name} id={todo.name} />
+          <span className='checkbox'></span>
+        </label>
+      </div>
+      <img src='/img/icon__close.svg' alt='delete icon' onClick={ () => deleteTodo(todo.id) } />
     </li>
   )
 }
