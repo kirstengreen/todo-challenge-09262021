2816c59a020774de406c3eb185f2d68359fff4b8
Kirsten Green <hello@kirstengreen.com>
2021-09-27 00:14:47 -0400

    refactored footer component and added styles

--- src/components/ListFooter/ListFooter.js
+++ src/components/ListFooter/ListFooter.js
@@ -5,12 +5,19 @@ export default function ListFooter( props ) {
   // Props: checked(for delete checked), notChecked(count not completed)
 
   return (
-    <div>
-      <div className="list_footer_options">
-        Options
+    <div className='footer_container'>
+      <div className='list_footer_options'>
+        <div className="items_left">
+          # items left
+        </div>
+        <div className='list_action'>
+          Clear completed
+        </div>
       </div>
-      <div className="list_footer_filters">
-        Filters
+      <div className='list_footer_filters'>
+        <div className='list_action'>All</div>
+        <div className='list_action'>Active</div>
+        <div className='list_action'>Completed</div>
       </div>
     </div>
   )
