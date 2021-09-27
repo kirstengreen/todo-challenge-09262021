d484ff5d2e7a821eaa695f7dd99e2e24186a0a50
Kirsten Green <hello@kirstengreen.com>
2021-09-26 21:34:12 -0400

    added header background image gradient to layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,8 +2,11 @@ import './Layout.css'
 
 export default function Layout( {children} ) {
   return(
-    <div className='page_container'>
-      {children}
+    <div>
+      <div className='page_header_bg'></div>
+      <div className='page_container'>
+        {children}
+      </div>
     </div>
   )
 }
