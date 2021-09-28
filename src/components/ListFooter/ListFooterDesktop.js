0d83c9fe4fd7c30f51a0c770e4af0b789e33ce88
Kirsten Green <hello@kirstengreen.com>
2021-09-28 15:54:35 -0400

    removed unneeded fragment component from desktop list footer

--- src/components/ListFooter/ListFooterDesktop.js
+++ src/components/ListFooter/ListFooterDesktop.js
@@ -1,21 +1,17 @@
-import { Fragment } from 'react'
-
 export default function ListFooterDesktop( {deleteCompleted} ) {
   return (
-    <Fragment>
-      <div className='list_footer_options'>
-        <div className="items_left">
-          # items left
-        </div>
-        <div className='list_footer_filters'>
-          <div className='list_action'>All</div>
-          <div className='list_action'>Active</div>
-          <div className='list_action'>Completed</div>
-        </div>
-        <div className='list_action' onClick={ () => deleteCompleted() }>
-          Clear completed
-        </div>
+    <div className='list_footer_options'>
+      <div className="items_left">
+        # items left
       </div>
-    </Fragment>
+      <div className='list_footer_filters'>
+        <div className='list_action'>All</div>
+        <div className='list_action'>Active</div>
+        <div className='list_action'>Completed</div>
+      </div>
+      <div className='list_action' onClick={ () => deleteCompleted() }>
+        Clear completed
+      </div>
+    </div>
   )
 }
