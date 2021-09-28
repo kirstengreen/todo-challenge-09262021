ba8756fefd16619bccb5a2b742089ab8000e93a2
Kirsten Green <hello@kirstengreen.com>
2021-09-28 15:26:10 -0400

    created veiwport state with listener function and setup logic for footer component

--- src/components/ListFooter/ListFooter.js
+++ src/components/ListFooter/ListFooter.js
@@ -1,22 +1,44 @@
+import { Fragment } from 'react'
 import './ListFooter.css'
 
-export default function ListFooter( {deleteCompleted} ) {
+export default function ListFooter( {deleteCompleted, viewportWidth, mobileBreakpoint} ) {
 
   return (
     <div className='footer_container'>
-      <div className='list_footer_options'>
-        <div className="items_left">
-          # items left
-        </div>
-        <div className='list_action' onClick={ () => deleteCompleted() }>
-          Clear completed
-        </div>
-      </div>
-      <div className='list_footer_filters'>
-        <div className='list_action'>All</div>
-        <div className='list_action'>Active</div>
-        <div className='list_action'>Completed</div>
-      </div>
-    </div>
+      { viewportWidth < mobileBreakpoint ? (
+        <Fragment>
+          <div className='list_footer_options'>
+            <div className="items_left">
+              # items left
+            </div>
+            <div className='list_action' onClick={ () => deleteCompleted() }>
+              Clear completed
+            </div>
+          </div>
+          <div className='list_footer_filters__mobile'>
+            <div className='list_action'>All</div>
+            <div className='list_action'>Active</div>
+            <div className='list_action'>Completed</div>
+          </div>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <div className='list_footer_options'>
+            <div className="items_left">
+              # items left
+            </div>
+            <div className='list_footer_filters'>
+              <div className='list_action'>All</div>
+              <div className='list_action'>Active</div>
+              <div className='list_action'>Completed</div>
+            </div>
+            <div className='list_action' onClick={ () => deleteCompleted() }>
+              Clear completed
+            </div>
+          </div>
+        </Fragment>
+      )
+    }
+  </div>
   )
 }
