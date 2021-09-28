90ffe72b4a81e28bb531be62739091b1ff8eb4cb
Kirsten Green <hello@kirstengreen.com>
2021-09-28 15:52:20 -0400

    created components for list footer mobile and desktop views

--- src/components/ListFooter/ListFooter.js
+++ src/components/ListFooter/ListFooter.js
@@ -1,42 +1,15 @@
-import { Fragment } from 'react'
 import './ListFooter.css'
+import ListFooterMobile from './ListFooterMobile'
+import ListFooterDesktop from './ListFooterDesktop'
 
 export default function ListFooter( {deleteCompleted, viewportWidth, mobileBreakpoint} ) {
 
   return (
     <div className='footer_container'>
       { viewportWidth < mobileBreakpoint ? (
-        <Fragment>
-          <div className='list_footer_options'>
-            <div className="items_left">
-              # items left
-            </div>
-            <div className='list_action' onClick={ () => deleteCompleted() }>
-              Clear completed
-            </div>
-          </div>
-          <div className='list_footer_filters__mobile'>
-            <div className='list_action'>All</div>
-            <div className='list_action'>Active</div>
-            <div className='list_action'>Completed</div>
-          </div>
-        </Fragment>
+        <ListFooterMobile deleteCompleted={deleteCompleted} />
       ) : (
-        <Fragment>
-          <div className='list_footer_options'>
-            <div className="items_left">
-              # items left
-            </div>
-            <div className='list_footer_filters'>
-              <div className='list_action'>All</div>
-              <div className='list_action'>Active</div>
-              <div className='list_action'>Completed</div>
-            </div>
-            <div className='list_action' onClick={ () => deleteCompleted() }>
-              Clear completed
-            </div>
-          </div>
-        </Fragment>
+        <ListFooterDesktop deleteCompleted={deleteCompleted} />
       )
     }
   </div>
