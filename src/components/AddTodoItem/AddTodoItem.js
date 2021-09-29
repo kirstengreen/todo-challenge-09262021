d180cd67d82abb7f9af56fec531b3c1fbcc6db98
Kirsten Green <hello@kirstengreen.com>
2021-09-29 11:03:48 -0400

    created add todo and onsubmit functions to setup add todo functionality

--- src/components/AddTodoItem/AddTodoItem.js
+++ src/components/AddTodoItem/AddTodoItem.js
@@ -1,14 +1,32 @@
+import { useState } from 'react'
 import './AddTodoItem.css'
 
-export default function AddListItem( {placeholderText, viewportWidth, mobileBreakpoint} ) {
+export default function AddListItem( {addTodo, placeholderText, viewportWidth, mobileBreakpoint} ) {
 
-  // Props: placeholderText
+  const [ name, setName ] = useState('')
+  const [ complete ] = useState(false)
+
+
+  // HANDLE SUBMIT
+  const onSubmit = (event) => {
+    event.preventDefault()
+
+    if( name === '' ) {
+      return
+    }
+
+    addTodo( {name, complete} )
+    setName('')
+  }
+
 
   return(
-    <form className='add_todo_form'>
+    <form className='add_todo_form' onSubmit={onSubmit}>
       <input
         className={`add_todo_input ${ viewportWidth > mobileBreakpoint ? 'add_todo_input__desktop' : '' }`}
+        value={name}
         placeholder={placeholderText} 
+        onChange={ (event) => setName(event.target.value) }
       />
     </form>
   )
